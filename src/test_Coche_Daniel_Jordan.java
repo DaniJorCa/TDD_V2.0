e62ae9543a7f77d255e64ad12df9748aaf188386
Dani <daniel.jorca@gmail.com>
2023-04-01 12:59:51 +0200

    Test decelerar velocidad

--- src/test_Coche_Daniel_Jordan.java
+++ src/test_Coche_Daniel_Jordan.java
@@ -7,14 +7,23 @@ import org.junit.Test;
 public class test_Coche_Daniel_Jordan {
 
 	@Test
-	public void test_al_crear_un_coche_su_velocidad_es_cero() {
+	public void test_al_crear_un_coche_su_velocidad_es_cero_Daniel_Jordan() {
 		Coche nuevoCoche = new Coche();
 		Assert.assertEquals(0, nuevoCoche.velocidad);
 	}
 	@Test
-	public void test_al_acelerar_un_coche_su_velocidad_aumenta() {
+	public void test_al_acelerar_un_coche_su_velocidad_aumenta_Daniel_Jordan() {
 		Coche nuevoCoche = new Coche();
 		nuevoCoche.acelerar(30);
+		Assert.assertEquals(30,nuevoCoche.velocidad);
+	}
+	
+	@Test 
+	
+	public void test_al_decelerar_un_coche_su_velocidad_disminuye_Daniel_Jordan() {
+		Coche nuevoCoche = new Coche();
+		nuevoCoche.velocidad = 50;
+		nuevoCoche.decelerar(20);
 		Assert.assertEquals(30,nuevoCoche.velocidad);
 	}
 	
